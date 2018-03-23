52a111ac0d54791908e866921e8611d58abcc41d
surinkim <nnhope@hotmail.com>
2018-03-23 22:36:33 +0900

    Add Chapter08/07_spread_fetching_web.py

---
+++ Chapter08/06_tooz-join-partitioned-group.py
@@ -0,0 +1,32 @@
+import sys
+import time
+
+from tooz import coordination
+
+# client id와 group id가 인수로 전달됐는지 확인.
+if len(sys.argv) != 3:
+    print("Usage: %s <client id> <group id>" % sys.argv[0])
+    sys.exit(1)
+
+# Coordinator 객체 얻음
+c = coordination.get_coordinator(
+    "memcached://localhost",
+    sys.argv[1].encode())
+# Coordinator 시작(연결 시작)
+c.start(start_heart=True)
+
+group = sys.argv[2].encode()
+
+# 파티션 그룹에 참가
+p = c.join_partitioned_group(group)
+
+print(p.members_for_object("foobar"))
+
+# 5초간 대기
+time.sleep(5)
+
+# 그룹 나가기
+c.leave_group(group).get()
+
+# 프로그램이 끝나면 Coordinator도 종료
+c.stop()
---
+++ Chapter08/07_spread_fetching_web.py
@@ -0,0 +1,48 @@
+import itertools
+import uuid
+
+import requests
+from tooz import coordination
+
+
+class URL(str):
+    def __tooz_hash__(self):
+        # URL을 고유 식별자로 사용한다.
+        return self.encode()
+
+urls_to_fetch = [
+    # N 바이트를 반환한다.
+    # 여기서 바이트 수는 URL 끝의 숫자다. 
+    URL("https://httpbin.org/bytes/%d" % n)
+    for n in range(100)
+]
+
+GROUP_NAME = b"fetcher"
+MEMBER_ID = str(uuid.uuid4()).encode('ascii')
+
+# Coordinator 객체 얻음
+c = coordination.get_coordinator("memcached://localhost", MEMBER_ID)
+# Coordinator 시작(연결 시작)
+c.start(start_heart=True)
+
+# 파티션 그룹에 참가
+p = c.join_partitioned_group(GROUP_NAME)
+
+try:
+    for url in itertools.cycle(urls_to_fetch):
+        # 멤버십 변경이 없는지 확인
+        c.run_watchers()
+        # print("%s -> %s" % (url, p.members_for_object(url)))
+        if p.belongs_to_self(url):
+            try:
+                r = requests.get(url)
+            except Exception:
+                # 에러가 발생하면
+                # 단순히 다음 아이템으로 이동
+                pass
+            else:
+                print("%s: fetched %s (%d)"
+                     % (MEMBER_ID, r.url, r.status_code))
+finally:
+    # 그룹 나가기
+    c.leave_group(GROUP_NAME).get()
