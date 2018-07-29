42b7db4d97ad1f7c793011c2f2d30e3ac51d6222
surinkim <nnhope@hotmail.com>
2018-07-29 11:28:22 +0900

    Update Chapter09/01_WSGI-basic.py

--- Chapter09/01_WSGI-basic.py
+++ Chapter09/01_WSGI-basic.py
@@ -3,4 +3,4 @@ def application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return ['Hello world!\n']
+    return [b'Hello world!\n']
